d2c6fee151347fab933dda7b2a3ca752085d1e63
jan.binkowski96 <jan.binkowski96@gmail.com>
2022-10-13 19:56:38 +0200

    Added mnt page

--- app/app.py
+++ app/app.py
@@ -10,6 +10,7 @@ with open("config.json", "r", encoding="UTF-8") as config_file:
 debug = config["debug"]
 version = config["version"]
 link = config["footer_link"]
+maintenance_page = config["maintenance_page"]
 
 logging.basicConfig(
     format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
@@ -18,45 +19,59 @@ logging.basicConfig(
     datefmt="%m/%d/%Y %I:%M:%S %p",
 )
 
-app = dash.Dash(__name__, external_stylesheets=[dbc.themes.FLATLY], use_pages=True)
-server = app.server
+if maintenance_page:
+    msg = """
+    eDAVE is currently not available due to maintaining works, it will come back as soon as possible.
+    """
+    app = dash.Dash(__name__, external_stylesheets=[dbc.themes.FLATLY], use_pages=False)
+    app.layout = dbc.Container(dash.html.H5(msg), fluid=True)
+    server = app.server
 
-pages = [dbc.ListGroupItem(page["name"], href=page["path"]) for page in dash.page_registry.values()]
-navbar = dbc.NavbarSimple(
-    children=[
-        dbc.NavItem(dbc.NavLink("Home", href="/")),
-        dbc.DropdownMenu(
-            children=[dbc.DropdownMenuItem("Pages", header=True), *pages],
-            nav=True,
-            in_navbar=True,
-            label="More",
-        ),
-    ],
-    brand=f"eDAVE v{version}",
-    brand_href="/",
-    color="primary",
-    dark=True,
-)
+else:
+    app = dash.Dash(__name__, external_stylesheets=[dbc.themes.FLATLY], use_pages=True)
+    server = app.server
 
-footer = dash.html.Footer(
-    [
-        dbc.Alert(
-            [
-                "Powered by ",
-                dash.html.A(
-                    "The Independent Clinical Epigenetics Laboratory",
-                    href=link,
-                    target="blank",
-                    className="alert-link",
-                ),
-                ", for research use only.",
-            ],
-            color="primary",
-        ),
-    ],
-    style={"bottom": "0", "width": "98%", "position": "fixed"},
-)
-app.layout = dbc.Container([navbar, dash.page_container, footer], fluid=True)
+    pages = [
+        dbc.ListGroupItem(page["name"], href=page["path"]) for page in dash.page_registry.values()
+    ]
+
+    navbar = dbc.NavbarSimple(
+        children=[
+            dbc.NavItem(dbc.NavLink("Home", href="/")),
+            dbc.DropdownMenu(
+                children=[dbc.DropdownMenuItem("Pages", header=True), *pages],
+                nav=True,
+                in_navbar=True,
+                label="More",
+            ),
+        ],
+        brand=f"eDAVE v{version}",
+        brand_href="/",
+        color="primary",
+        dark=True,
+    )
+
+    footer = dash.html.Footer(
+        [
+            dbc.Alert(
+                [
+                    "Powered by ",
+                    dash.html.A(
+                        "The Independent Clinical Epigenetics Laboratory",
+                        href=link,
+                        target="blank",
+                        className="alert-link",
+                    ),
+                    ", for research use only.",
+                ],
+                color="primary",
+            ),
+        ],
+        style={"bottom": "0", "width": "98%", "position": "fixed"},
+    )
+
+    app.layout = dbc.Container([navbar, dash.page_container, footer], fluid=True)
+
 
 if __name__ == "__main__":
     app.run_server(debug=debug)
