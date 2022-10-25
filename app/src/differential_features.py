1a3c02e26914bd80b2eb011b439f9b2bc41f47e8
jan.binkowski96 <jan.binkowski96@gmail.com>
2022-10-25 11:15:44 +0200

    Added alpha and effect to class atirbutes

--- app/src/differential_features.py
+++ app/src/differential_features.py
@@ -1,5 +1,3 @@
-import os
-
 import numpy as np
 import pandas as pd
 import scipy.stats as sts
@@ -7,15 +5,29 @@ from statsmodels.stats.multitest import fdrcorrection
 
 
 class DifferentialFeatures:
-    def __init__(self, data_frame: pd.DataFrame, samples: pd.Series, group_A: str, group_B: str):
+    def __init__(
+        self,
+        data_type: str,
+        data_frame: pd.DataFrame,
+        samples: pd.Series,
+        group_A: str,
+        group_B: str,
+        alpha: float,
+        effect_size: float,
+    ):
+        self.data_type = data_type
         self.data_frame = data_frame
-        self.variables = self.data_frame.index
+        self.group_A = group_A
+        self.group_B = group_B
         self.samples_A = samples[samples == group_A].index
         self.samples_B = samples[samples == group_B].index
-        self.records = []
+        self.variables = self.data_frame.index
+        self.alpha = alpha
+        self.effect_size = effect_size
         self.stats_frame = None
+        self.records = []
 
-    def run(self, alpha: float = 0.05) -> None:
+    def identify_differential_features(self) -> None:
         group_a = self.data_frame[self.samples_A]
         group_b = self.data_frame[self.samples_B]
 
@@ -27,11 +39,11 @@ class DifferentialFeatures:
             _, norm_b = sts.shapiro(group_b_temp)
             _, var_a_b = sts.levene(group_a_temp, group_b_temp)
 
-            if norm_a > alpha and norm_b > alpha and var_a_b > alpha:
+            if norm_a > self.alpha and norm_b > self.alpha and var_a_b > self.alpha:
                 status = "parametric"
                 _, diff_pvalue = sts.ttest_ind(group_a_temp, group_b_temp, equal_var=True)
 
-            elif norm_a > alpha >= var_a_b and norm_b > alpha:
+            elif norm_a > self.alpha >= var_a_b and norm_b > self.alpha:
                 status = "parametric - not equal variance"
                 _, diff_pvalue = sts.ttest_ind(group_a_temp, group_b_temp, equal_var=False)
 
@@ -43,16 +55,20 @@ class DifferentialFeatures:
             group_a_temp_mean = np.mean(group_a_temp)
             group_b_temp_mean = np.mean(group_b_temp)
 
-            fc = group_a_temp_mean / group_b_temp_mean
-            log_fc = np.log2(fc)
+            if group_b_temp_mean != 0:
+                fc = group_a_temp_mean / group_b_temp_mean
+                log_fc = np.log2(fc)
+            else:
+                fc = np.NaN
+                log_fc = np.NaN
 
             delta = group_a_temp_mean - group_b_temp_mean
             abs_delta = abs(delta)
 
             record = {
                 "Feature": var,
-                "Mean(A)": group_a_temp_mean,
-                "Mean(B)": group_b_temp_mean,
+                f"Mean({self.group_A})": group_a_temp_mean,
+                f"Mean({self.group_B})": group_b_temp_mean,
                 "FC": fc,
                 "log2(FC)": log_fc,
                 "delta": delta,
@@ -66,15 +82,25 @@ class DifferentialFeatures:
 
     def build_statistics_frame(self) -> None:
         frame = pd.DataFrame(self.records)
+
         _, frame["FDR"] = fdrcorrection(frame["p-value"])
-        frame["Rank"] = frame["FDR"].map(lambda value: abs(np.log10(value))) * frame["|delta|"]
+        frame["-log10(FDR)"] = frame["FDR"].map(lambda value: -np.log10(value))
+        frame["Rank"] = frame["-log10(FDR)"] * frame["log2(FC)"].abs()
         frame["Rank"] = frame["Rank"] / frame["Rank"].sum()
 
         frame = frame.sort_values("Rank", ascending=False)
         frame = frame.set_index("Feature")
+
+        if self.data_type == "Expression [RNA-seq]":
+            frame["DEG/DMP"] = (frame["FDR"] <= self.alpha) & (
+                frame["log2(FC)"].abs() > self.effect_size
+            )
+        else:
+            frame["DEG/DMP"] = (frame["FDR"] <= self.alpha) & (
+                frame["delta"].abs() > self.effect_size
+            )
+
         self.stats_frame = frame
 
-    def export(self, data_type: str, group_A: str, group_B: str) -> None:
-        path = f"temp/{data_type.replace('/', '_')}_{group_A}_{group_B}.parquet"
-        os.makedirs("temp/", exist_ok=True)
+    def export(self, path: str) -> None:
         self.stats_frame.to_parquet(path)
