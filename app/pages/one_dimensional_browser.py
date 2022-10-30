0a5704b60a91b5506757ef40a1db4c517854ea81
jan.binkowski96 <jan.binkowski96@gmail.com>
2022-10-30 17:39:16 +0100

    new example

--- app/pages/one_dimensional_browser.py
+++ app/pages/one_dimensional_browser.py
@@ -239,7 +239,7 @@ def update_input_field(data_type: str) -> t.Tuple[bool, str]:
     Function to update input field.
     """
     if data_type == "Expression [RNA-seq]":
-        return False, "E.g. PAX3"
+        return False, "E.g. BRCA1"
     if data_type == "Methylation [450K/EPIC]":
         return False, "E.g. cg07779434"
 
