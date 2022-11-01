252945348d1b0c04cb3e3c242b7eff7541298a66
jan.binkowski96 <jan.binkowski96@gmail.com>
2022-11-01 13:42:20 +0100

    Change y-axis in volcano plot

--- app/pages/differential_features_browser.py
+++ app/pages/differential_features_browser.py
@@ -295,10 +295,10 @@ def main_dfeatures_browser(
         results = diffF.stats_frame
 
         if data_type == "Expression [RNA-seq]":
-            plot = Plot(results, "log2(FC)", "-log10(p-value)", None, None)
+            plot = Plot(results, "log2(FC)", "-log10(FDR)", None, None)
             fig = plot.volcanoplot(x_border=effect_size, y_border=-np.log10(alpha))
         else:
-            plot = Plot(results, "delta", "-log10(p-value)", None, None)
+            plot = Plot(results, "delta", "-log10(FDR)", None, None)
             fig = plot.volcanoplot(x_border=effect_size, y_border=-np.log10(alpha))
 
         count = Stats(sample_frame.to_frame(), "SampleType").get_factor_count
