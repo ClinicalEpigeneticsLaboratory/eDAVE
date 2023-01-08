a14e908c2ca2e95a38e7105f4fe44d57ad323ee3
jan.binkowski96 <jan.binkowski96@gmail.com>
2023-01-08 21:03:48 +0100

    Updated docs

--- app/pages/documentation.py
+++ app/pages/documentation.py
@@ -57,7 +57,7 @@ layout = dbc.Container(
     #### CpG identifiers
 
     All CpGs targeted by 450K (n≈450.000) and EPIC (n≈850.000) microarrays have a unique identifier, for example cg22930808.
-    Manifests containing all of CpGs along with genomic context for specific technology are available from:
+    Manifests containing all of targeted CpGs along with genomic context for specific technology are available from:
 
     • [EPIC](https://support.illumina.com/downloads/infinium-methylationepic-v1-0-product-files.html)
 
@@ -69,7 +69,6 @@ layout = dbc.Container(
     ---
 
     #### Raw data processing pipelines
-
 
     Raw data processing pipelines are in details described by GDC:
 
