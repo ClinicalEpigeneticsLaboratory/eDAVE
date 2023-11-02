c05d24e49a899f4a156454e26fef5f5eae563223
JBinkowski <jan.binkowski96@gmail.com>
2023-11-02 14:42:36 +0100

    Fixed DEG/DMP type

--- app/src/plots.py
+++ app/src/plots.py
@@ -133,17 +133,20 @@ class Plot:
 
         :return Fig:
         """
-        names = self.data.index
+        data = self.data
+        data["DEG/DMP"] = data["DEG/DMP"].astype(str)
+
+        names = data.index
         names.name = "Feature"
 
         fig = px.scatter(
-            data_frame=self.data,
+            data_frame=data,
             x=self.x_axis,
             y=self.y_axis,
             hover_data=[names, self.data.FC, self.data.delta, self.data["Hedge`s g"]],
             color="DEG/DMP",
-            color_discrete_map={True: "red", False: "blue"},
-            category_orders={"DEG/DMP": [True, False]}
+            color_discrete_map={"True": "red", "False": "blue"},
+            category_orders={"DEG/DMP": ["True", "False"]}
         )
 
         fig.update_layout(font={"size": self.font_size})
@@ -161,7 +164,7 @@ class Plot:
         fig = px.pie(
             values=data.values,
             names=data.index,
-            category_orders={"DEG/DMP": [True, False]},
+            category_orders={"DEG/DMP": ["True", "False"]},
             color_discrete_map={"True": "red", "False": "blue"},
             labels={"value": "Count", "index": "DEG/DMP"},
         )
